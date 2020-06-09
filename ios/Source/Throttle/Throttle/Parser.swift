5afac591f00b22241527a7491acbdb7302b4e3f1
ShevaKuilin <shevakuilin@gmail.com>
2020-06-09 15:42:37 +0800

    Modify: Parser.swift add struct content

--- ios/Source/Throttle/Throttle/Parser.swift
+++ ios/Source/Throttle/Throttle/Parser.swift
@@ -19,22 +19,47 @@
 //
   
 
-import Foundation
+import UIKit
 
 /// DSL 解析器
-struct Parser {}
+public struct Parser {
+    
+}
 
 /// DSL定义
-private struct DSL {}
+private struct DSL {
+    fileprivate var layoutStyle: LayoutStyle?
+    fileprivate var viewStyle: ViewStyle?
+    fileprivate var viewAction: ViewAction?
+    fileprivate var condition: Condition?
+}
 
 /// 布局方式
-private struct LayoutStyle {}
+private struct LayoutStyle {
+    fileprivate enum style {
+        case frame
+        case flexbox
+    }
+}
 
 /// 视图属性
-private struct ViewStyle {}
+private struct ViewStyle {
+    fileprivate var type: AnyClass?
+    fileprivate var backgroundColor: UIColor = .white
+    fileprivate var alpha: CGFloat = 1.0
+    fileprivate var cornerRadius: CGFloat = 0.0
+    fileprivate var borderColor: UIColor?
+    fileprivate var borderWidth: CGFloat = 0.0
+    fileprivate var frame: CGRect = .zero
+}
 
 /// 交互属性
-private struct ViewAction {}
+private struct ViewAction {
+    fileprivate var action: Selector?
+}
 
 /// 视图状态条件
-private struct Condition {}
+private struct Condition {
+    fileprivate var parentView: AnyObject?
+    fileprivate var subView: AnyObject?
+}
